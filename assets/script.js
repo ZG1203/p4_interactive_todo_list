60b03e97b17a41e3b957f29d367dca99f20bad83
ZG1203 <z0395621@gmail.com>
2025-10-12 16:31:22 +0100

    inital commit - UI without function

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// 1. add button 
+// when click add button, check input value, add record to to do list, generate a sequential number for task ID
+
+
+// 2. complete toggle(button)
+// when click on the button, switch between yes and no, with different background color
+
+// 3. delete button
+// when click delete button, delete the record from list
+
+// 4. edit button
+// when click edit button, allow user to update due date and/or details 
